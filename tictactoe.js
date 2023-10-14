ce6eefc6f1e556f150719f235952dace43d60672
Chad Dore <chaduke@gmail.com>
2023-10-13 21:23:55 -0500

    New sound and a few corrections

--- tictactoe.js
+++ tictactoe.js
@@ -1,20 +1,29 @@
 // TODO
-
-// create a game state machine
+// draw winner line
+// redo victory formula
 // study text alignment
 // build up moves log
 // store moves in mongo
 // build a computer opponent
 // build a network opponent system
+// play different sound for tie game
+// organize screen with css
+// play music for game start / play / end
+
+// state machine constants
+const GAME_STATE_START = 0;
+const GAME_STATE_PLAY = 1;
+const GAME_STATE_END = 2;
 
 function setup() 
 {
-  screen_size = 400;
+  screen_size = 600;
   sidebar_width = Math.floor(screen_size * 0.334);  
   createCanvas(screen_size + sidebar_width, screen_size);
   
   // console.log("Initializing screen:" + width + "," + height);
   // console.log("Sidebar width:" + sidebar_width);
+
   label_height = Math.floor(height * 0.067);
   // console.log("Label height:" + label_height);   
   
@@ -31,75 +40,83 @@ function setup()
     }
   console.log("Columns:" + columns.substr(0,columns.length-1));
   console.log("Rows:" + rows.substr(0,rows.length-1));
-  */  
-  
-  snd_switch = loadSound("assets/switch22.ogg");
-  snd_switch2 = loadSound("assets/switch26.ogg");
-  snd_gameEnd = loadSound("assets/threeTone2.ogg");
-  init();
+  */
+  snd_hover = loadSound("assets/switch22.ogg");
+  snd_play = loadSound("assets/switch26.ogg");
+  snd_gameWin = loadSound("assets/threeTone2.ogg"); 
+  snd_gameTie = loadSound("assets/lowThreeTone.ogg"); 
+  gameState = GAME_STATE_START;  
 }
 
 function init()
-{
+{    
   moves = '         ';
   // moves = 'xo xo xo ';
   // console.log("Board initialized to:" + moves);
   lastCell = 0;
-  gameStart = true;
-  gameEnd = false;
+  
   player = 'x';
   movecount = 0;
 }
 
 function draw() 
 {
-  if (gameStart) 
+  switch(gameState)
   {
-    fill(222);
-    rect(0,0,width,height);
-    textFont('monospace');
-    textSize(move_fontsize * 0.25);
-    fill(64,222,64);
-    text("TIC TAC TOE",width * 0.25,height * 0.5);
-    textSize(move_fontsize * 0.2);
-    text("Click to play",width * 0.26,height * 0.6);
-  }
-  else
-  {
-    background('skyblue');      
-    draw_sidebar(); 
-    check_mouse();   
-    draw_board();
-    draw_moves();      
-  }  
+    case GAME_STATE_START:
+      fill(222);
+      rect(0,0,width,height);
+      textFont('monospace');
+      textSize(move_fontsize * 0.25);
+      fill(64,222,64);
+      text("TIC TAC TOE",width * 0.25,height * 0.5);
+      textSize(move_fontsize * 0.2);
+      text("Click to play",width * 0.26,height * 0.6);
+      break;
+    case GAME_STATE_PLAY:
+      background('skyblue');      
+      draw_sidebar(); 
+      check_mouse();   
+      draw_board();
+      draw_moves();
+      break;
+    case GAME_STATE_END:
+      background('skyblue');      
+      draw_sidebar(); 
+      check_mouse();   
+      draw_board();
+      draw_moves();
+      break;
+  }   
 }
 
 function mousePressed()
 {
-  if (gameEnd) 
+  if (gameState === GAME_STATE_END || gameState === GAME_STATE_START) 
   {
     init();
+    gameState = GAME_STATE_PLAY;
   }
-
-  if(gameStart)
+  else  
   {
-    gameStart = false;
-  }   
-  else
-  {
-    if (moves[cell]===' ' && !gameEnd)
+    if (moves[cell]===' ' && gameState!=GAME_STATE_END)
     {
       moves = moves.replaceAt(cell,player);
       movecount++;
       victory_check();
-      if (!gameEnd) 
+      if (gameState === GAME_STATE_PLAY) 
       {
         if (player==='x') player = 'o'; else player = 'x';
-        snd_switch2.play();
+        snd_play.play();
       }
       else
       {
-        snd_gameEnd.play();
+        if (player==='t')
+        {
+          snd_gameTie.play()
+        } else {
+          snd_gameWin.play();  
+        }  
       }        
     }
   }  
@@ -121,7 +138,7 @@ function check_mouse()
     // text('Row:' + row,5,label_height * 4);
     cell = col + row * 3;
     // text('Cell:' + cell,5,label_height * 5);
-    if (moves[cell]===' ' && !gameEnd)
+    if (moves[cell]===' ' && gameState!=GAME_STATE_END)
     {
       // light up cell
       x = col * sidebar_width + sidebar_width;
@@ -133,9 +150,9 @@ function check_mouse()
       textSize(move_fontsize * 0.15);
       text('Click to play here',x,y,sidebar_width,sidebar_width);
       // play sound
-      if (!snd_switch.isPlaying() && cell != lastCell)
+      if (!snd_hover.isPlaying() && cell != lastCell)
       {
-        snd_switch.play();
+        snd_hover.play();
         lastCell = cell;
       }
     }     
@@ -153,49 +170,49 @@ function victory_check()
   // row 1
   if(moves[0] === moves[1] && moves[1] === moves[2] && moves[0] != ' ')
   {
-    gameEnd = true;  
+    gameState = GAME_STATE_END;
   }
   // row 2
   if(moves[3] === moves[4] && moves[4] === moves[5] && moves[3] != ' ')
   {
-    gameEnd = true;  
+    gameState = GAME_STATE_END;  
   }
   // row 3
   if(moves[6] === moves[7] && moves[7] === moves[8] && moves[6] != ' ')
   {
-    gameEnd = true;  
+    gameState = GAME_STATE_END; 
   }
   // check vertical columns
   // col 1
   if(moves[0] === moves[3] && moves[3] === moves[6] && moves[0] != ' ')
   {
-    gameEnd = true;  
+    gameState = GAME_STATE_END;  
   }
   // col 2
   if(moves[1] === moves[4] && moves[4] === moves[7] && moves[1] != ' ')
   {
-    gameEnd = true;  
+    gameState = GAME_STATE_END; 
   }
   // col 3
   if(moves[2] === moves[5] && moves[5] === moves[8] && moves[2] != ' ')
   {
-    gameEnd = true;  
+    gameState = GAME_STATE_END;  
   }
   // check diagonal top left to bottom right  
   if(moves[0] === moves[4] && moves[4] === moves[8] && moves[0] != ' ')
   {
-    gameEnd = true;  
+    gameState = GAME_STATE_END;  
   }
   // check diagonal bottom left to top right
   if(moves[6] === moves[4] && moves[4] === moves[2] && moves[6] != ' ')
   {
-    gameEnd = true;  
+    gameState = GAME_STATE_END; 
   } 
   // check for tie
-  if(!gameEnd && movecount===9)
+  if(gameState===GAME_STATE_PLAY && movecount===9)
   {
-    gameEnd = true;
-    player = 't';  
+    player = 't';
+    gameState = GAME_STATE_END;
   }
 }
 
@@ -213,7 +230,7 @@ function draw_sidebar()
   y = label_height * 0.75;
   text(label,x,y);
   fill(0);
-  if (gameEnd)
+  if (gameState === GAME_STATE_END)
   {
     fill(255,64,64);
     if (player != 't')
